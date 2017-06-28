cabf311a306745d2aaea0cd7a5598ff1c2fd2741
James Illing <J.Illing@Craneware.com>
2017-06-28 11:40:52 +0100

    Added code and configuration.

---
+++ Sha/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace SecureHashAlgorithm
+{
+    class Configuration
+    {
+        [Option('h', "hash", Required = true, HelpText = "The cryptographic hash to use")]
+        public string Hash{ get; set; }
+
+
+        [Option('i', "input", Required = true, HelpText = "Input to encrypt")]
+        public string Input { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {            
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
--- Sha/Program.cs
+++ Sha/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Sha
+namespace SecureHashAlgorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new Configuration();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                HashAlgorithm hashAlgorithm;
+                switch (options.Hash.ToUpper())
+                {
+                    case "SHA1":
+                        hashAlgorithm = new SHA1Managed();
+                        break;
+                    case "SHA256":
+                        hashAlgorithm = new SHA256Managed();
+                        break;
+                    case "SHA384":
+                        hashAlgorithm = new SHA384Managed();
+                        break;
+                    case "SHA512":
+                        hashAlgorithm = new SHA512Managed();
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown Hash {options.Hash}");
+                        return;                        
+                }
+                var textToHash = options.Input;
+                if (string.IsNullOrEmpty(textToHash))
+                {
+                    Console.WriteLine("No data to hash");
+                    return;
+                }
+                var hash = hashAlgorithm.ComputeHash(Encoding.ASCII.GetBytes(textToHash));
+                Console.WriteLine(hash.ToHexString());        
+            }                           
+        }    
+    }
+
+    public static class Extension
+    {
+        public static string ToHexString(this byte[] data)
+        {
+            var hexString = new StringBuilder();
+            foreach (var b in data)
+            {
+                hexString.AppendFormat("{0:X2}", b);
+            }
+            return hexString.ToString();
         }
     }
 }
